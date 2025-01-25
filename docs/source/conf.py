755e1319df3c015e696d5cd32e569a655c0b5cb0
João Biu <joaopedrofbiu@tecnico.ulisboa.pt>
2025-01-25 02:27:10 +0000

    asda

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = 'v1.0.2'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx_rtd_theme"]
 
 templates_path = ['_templates']
 exclude_patterns = []
