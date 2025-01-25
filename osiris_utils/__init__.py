535e687d35708a72360b3d82a0a8f9aefaa9c0fb
João Biu <joaopedrofbiu@tecnico.ulisboa.pt>
2025-01-25 01:35:15 +0000

    prepare to merge

--- osiris_utils/__init__.py
+++ osiris_utils/__init__.py
@@ -2,5 +2,4 @@ from .utils import (time_estimation, filesize_estimation, transverse_average, in
                     save_data, read_data, mft_decomposition, courant2D)
 from .data_readers import create_dataset, open1D, open2D, open3D, read_osiris_file
 from .gui import LAVA
-from .analysis import TwoFluid2D
 from .data import OsirisGridFile
