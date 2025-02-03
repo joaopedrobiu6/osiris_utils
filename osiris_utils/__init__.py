d883ac7321355f9c10fa54911c72667d35dc6260
Joao Candido <joao.a.g.candido@tecnico.ulisboa.pt>
2025-02-03 15:26:44 +0000

    feat: Class to read raw data

--- osiris_utils/__init__.py
+++ osiris_utils/__init__.py
@@ -2,4 +2,4 @@ from .utils import (time_estimation, filesize_estimation, transverse_average, in
                     save_data, read_data, mft_decomposition, courant2D)
 from .data_readers import open1D, open2D, open3D, read_osiris_file
 from .gui import LAVA_Qt, LAVA
-from .data import OsirisGridFile
+from .data import OsirisGridFile, OsirisRawFile
--- osiris_utils/data.py
+++ osiris_utils/data.py
@@ -77,3 +77,69 @@ class OsirisGridFile():
             self.units = f.attrs["UNITS"][0].decode('utf-8')
             self.label = f.attrs["LABEL"][0].decode('utf-8')
             self.type = f.attrs["TYPE"][0].decode('utf-8')
+
+
+class OsirisRawFile():
+    '''
+    Class to read the raw data from an OSIRIS HDF5 file.
+    
+    Input:
+        - filename: the path to the HDF5 file
+        
+    Attributes:
+
+        - axis: A dictionary where each key is a dataset name, and each value is another dictionary containing:
+                name (str): The name of the quantity (e.g., 'x1', 'ene').
+                units (str): The units associated with that dataset in LaTeX (e.g., 'c/\\omega_p', 'm_e c^2').
+                long_name (str): The name of the quantity in LaTeX (e.g., 'x_1', 'En2').
+            dictionary of dictionaries
+        - dt: the time step
+            float
+        - dim: the number of dimensions
+            int
+        - time: the time and its units
+            list [time, units]
+            list [float, str]
+        - iter: the iteration number
+            int
+        - name: the name of the species
+            str
+        -type: type of data (particles in the case of raw files) 
+            str
+
+    Example:
+
+        electrons = utils.OsirisRawFile("MS/RAW/electrons/RAW-electrons-000002.h5")
+        plt.hist2d(electrons.data["x1"], electrons.data["x2"], bins=100, cmap='plasma', norm=mpl.colors.LogNorm())
+        plt.xlabel(r"${}$".format(electrons.axis["x1"]["long_name"]) + r"$\quad[{}]$".format(electrons.axis["x1"]["units"]))
+        plt.ylabel(r"${}$".format(electrons.axis["x2"]["long_name"]) + r"$\quad[{}]$".format(electrons.axis["x2"]["units"]))
+        plt.title(r"$t={}$".format(electrons.time[0]) + r"$\quad[{}]$".format(electrons.time[1]))
+        plt.show()
+    '''
+    
+    def __init__(self, filename):
+        self.filename = filename
+
+        with h5py.File(self.filename, "r") as f: 
+
+            self.dt = float(f["SIMULATION"].attrs["DT"][0])
+            self.dim = int(f["SIMULATION"].attrs["NDIMS"][0])
+            self.time = [float(f.attrs["TIME"][0]), f.attrs["TIME UNITS"][0].decode('utf-8')]
+            self.iter = int(f.attrs["ITER"][0])
+            self.name = f.attrs["NAME"][0].decode('utf-8')
+            self.type = f.attrs["TYPE"][0].decode('utf-8')
+
+            self.data = {}
+            self.axis = {}
+            for key in f.keys():
+                if key == "SIMULATION": continue
+
+                self.data[key] = np.array(f[key][()])
+
+                idx = np.where(f.attrs["QUANTS"] == str(key).encode('utf-8'))
+                axis_data = {
+                    "name": f.attrs["QUANTS"][idx][0].decode('utf-8'),
+                    "units": f.attrs["UNITS"][idx][0].decode('utf-8'),
+                    "long_name": f.attrs["LABELS"][idx][0].decode('utf-8'),
+                }
+                self.axis[key] = axis_data
