ebb7567eb92646b0252e926b4dead92ad2d9eb4e
João Biu <joaopedrofbiu@tecnico.ulisboa.pt>
2025-03-15 17:34:35 +0000

    deleted anomalous resistivity stuff

--- osiris_utils/__init__.py
+++ osiris_utils/__init__.py
@@ -4,4 +4,3 @@ from .data_readers import open1D, open2D, open3D, read_osiris_file
 from .gui import LAVA_Qt, LAVA
 from .data import OsirisGridFile, OsirisRawFile, OsirisData, OsirisHIST
 from .mean_field_theory import MeanFieldTheory
-from .anomalousresistivity import AnomalousResistivity, Omega_K
