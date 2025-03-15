d598d55ac34c42a195ebd3b875ed99f86e7f7c81
João Biu <joaopedrofbiu@tecnico.ulisboa.pt>
2025-03-15 00:07:54 +0000

    try now

--- osiris_utils/gui.py
+++ osiris_utils/gui.py
@@ -14,7 +14,7 @@ from .utils import integrate, transverse_average  # Update import as needed
 class LAVA_Qt(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("LAVA (LabAstro Visualization Assistant) - OSIRIS Data Grid Viewer")
+        self.setWindowTitle('LAVA (LabAstro Visualization Assistant) - OSIRIS Data Grid Viewer')
         self.setGeometry(100, 100, 1000, 600)
         
         # Initialize data
@@ -39,28 +39,28 @@ class LAVA_Qt(QMainWindow):
         control_layout = QHBoxLayout(control_frame)
         
         # Buttons
-        self.browse_btn = QPushButton("Browse Folder")
+        self.browse_btn = QPushButton('Browse Folder')
         self.browse_btn.clicked.connect(self.load_folder)
-        self.save_btn = QPushButton("Save Plot")
+        self.save_btn = QPushButton('Save Plot')
         self.save_btn.clicked.connect(self.save_plot)
         
         # File selector
         self.file_selector = QComboBox()
-        self.file_selector.setPlaceholderText("Select file...")
+        self.file_selector.setPlaceholderText('Select file...')
         self.file_selector.currentIndexChanged.connect(self.file_selection_changed)
         self.file_selector.view().setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.file_selector.setSizeAdjustPolicy(QComboBox.AdjustToContents)
         
         # Plot type combo box
         self.plot_combo = QComboBox()
-        self.plot_combo.addItem("Select Plot Type")
+        self.plot_combo.addItem('Select Plot Type')
         self.plot_combo.currentTextChanged.connect(self.plot_data)
         
         control_layout.addWidget(self.browse_btn)
         control_layout.addWidget(self.save_btn)
-        control_layout.addWidget(QLabel("Files:"))
+        control_layout.addWidget(QLabel('Files:'))
         control_layout.addWidget(self.file_selector)
-        control_layout.addWidget(QLabel("Plot Type:"))
+        control_layout.addWidget(QLabel('Plot Type:'))
         control_layout.addWidget(self.plot_combo)
         self.main_layout.addWidget(control_frame)
 
@@ -80,11 +80,11 @@ class LAVA_Qt(QMainWindow):
         self.ylabel_edit.textChanged.connect(self.update_plot_labels)
         
         
-        labels_layout.addWidget(QLabel("Title:"))
+        labels_layout.addWidget(QLabel('Title:'))
         labels_layout.addWidget(self.title_edit)
-        labels_layout.addWidget(QLabel("X Label:"))
+        labels_layout.addWidget(QLabel('X Label:'))
         labels_layout.addWidget(self.xlabel_edit)
-        labels_layout.addWidget(QLabel("Y Label:"))
+        labels_layout.addWidget(QLabel('Y Label:'))
         labels_layout.addWidget(self.ylabel_edit)
         
         # define the size of the labels frame
@@ -94,7 +94,7 @@ class LAVA_Qt(QMainWindow):
     def load_folder(self):
         folder_dialog = QFileDialog()
         folderpath = folder_dialog.getExistingDirectory(
-            self, "Select Folder with HDF5 Files"
+            self, 'Select Folder with HDF5 Files'
         )
         
         if not folderpath:
@@ -119,16 +119,16 @@ class LAVA_Qt(QMainWindow):
             h5_files.sort(key=sort_key)
             
             if not h5_files:
-                raise ValueError("No HDF5 files found in selected folder")
+                raise ValueError('No HDF5 files found in selected folder')
             
             self.file_selector.addItems(h5_files)
             self.file_selector.setCurrentIndex(0)
             
         except Exception as e:
-            QMessageBox.critical(self, "Error", str(e))
+            QMessageBox.critical(self, 'Error', str(e))
 
     def file_selection_changed(self, index):
-        """Handle file selection change in the combo box"""
+        '''Handle file selection change in the combo box'''
         if index >= 0 and self.current_folder:
             filename = self.file_selector.itemText(index)
             self.process_file(filename)
@@ -140,32 +140,32 @@ class LAVA_Qt(QMainWindow):
             self.dims = len(gridfile.axis)
             self.type = gridfile.type
             
-            if self.type == "grid":
+            if self.type == 'grid':
                 if self.dims == 1:
                     x = np.arange(gridfile.grid[0], gridfile.grid[1], gridfile.dx)
-                    self.xlabel_edit.setText(r"$%s$ [$%s$]" % (gridfile.axis[0]["long_name"], gridfile.axis[0]["units"]))
-                    self.ylabel_edit.setText(r"$%s$ [$%s$]" % (gridfile.label, gridfile.units))
+                    self.xlabel_edit.setText(r'$%s$ [$%s$]' % (gridfile.axis[0]['long_name'], gridfile.axis[0]['units']))
+                    self.ylabel_edit.setText(r'$%s$ [$%s$]' % (gridfile.label, gridfile.units))
                     self.data_info = (x, gridfile.data)
                 elif self.dims == 2:
                     x = np.arange(gridfile.grid[0][0], gridfile.grid[0][1], gridfile.dx[0])
                     y = np.arange(gridfile.grid[1][0], gridfile.grid[1][1], gridfile.dx[1])
-                    self.xlabel_edit.setText(r"$%s$ [$%s$]" % (gridfile.axis[0]["long_name"], gridfile.axis[0]["units"]))
-                    self.ylabel_edit.setText(r"$%s$ [$%s$]" % (gridfile.axis[1]["long_name"], gridfile.axis[1]["units"]))
+                    self.xlabel_edit.setText(r'$%s$ [$%s$]' % (gridfile.axis[0]['long_name'], gridfile.axis[0]['units']))
+                    self.ylabel_edit.setText(r'$%s$ [$%s$]' % (gridfile.axis[1]['long_name'], gridfile.axis[1]['units']))
                     self.data_info = (x, y, gridfile.data)
                 elif self.dims == 3:
-                    raise ValueError("3D not supported yet")
+                    raise ValueError('3D not supported yet')
                 else:
-                    raise ValueError("Unsupported dimensionality")
+                    raise ValueError('Unsupported dimensionality')
                 
-                self.title_edit.setText(r"$%s$ [$%s$]" %( gridfile.label, gridfile.units))
+                self.title_edit.setText(r'$%s$ [$%s$]' %( gridfile.label, gridfile.units))
                 self.update_plot_menu()
                 self.plot_data()
                 
             else:
-                QMessageBox.information(self, "Info", f"{self.type} data not supported yet")
+                QMessageBox.information(self, 'Info', f'{self.type} data not supported yet')
                 
         except Exception as e:
-            QMessageBox.critical(self, "Error", str(e))
+            QMessageBox.critical(self, 'Error', str(e))
 
     def create_plot_area(self):
         # Matplotlib figure and canvas
@@ -194,9 +194,9 @@ class LAVA_Qt(QMainWindow):
         self.current_ax = self.figure.add_subplot(111)
         plot_type = self.plot_combo.currentText()
         
-        if "Line" in plot_type:
+        if 'Line' in plot_type:
             self.current_ax.plot(x, data)
-        elif "Scatter" in plot_type:
+        elif 'Scatter' in plot_type:
             self.current_ax.scatter(x, data)
             
         self.current_ax.set_xlabel(self.xlabel_edit.text())
@@ -208,16 +208,16 @@ class LAVA_Qt(QMainWindow):
         self.current_ax = self.figure.add_subplot(111)
         plot_type = self.plot_combo.currentText()
         
-        if "Quantity" in plot_type:
+        if 'Quantity' in plot_type:
             img = self.current_ax.imshow(data.T, extent=(x[0], x[-1], y[0], y[-1]), origin='lower', aspect='auto')
             self.figure.colorbar(img)
-        elif "Integral" in plot_type:
+        elif 'Integral' in plot_type:
             avg = integrate(transverse_average(data), x[-1]/len(x))
             self.current_ax.plot(x, avg)
-        elif "Transverse" in plot_type:
+        elif 'Transverse' in plot_type:
             avg = transverse_average(data)
             self.current_ax.plot(x, avg)
-        elif "Phase" in plot_type:
+        elif 'Phase' in plot_type:
             img = self.current_ax.imshow(np.abs(-data.T), extent=(x[0], x[-1], y[0], y[-1]), origin='lower', aspect='auto', norm=LogNorm())
             self.figure.colorbar(img)
         
@@ -233,9 +233,9 @@ class LAVA_Qt(QMainWindow):
         
         # Determine items based on dimensions
         if self.dims == 1:
-            items = ["Line Plot", "Scatter Plot"]
+            items = ['Line Plot', 'Scatter Plot']
         elif self.dims == 2:
-            items = ["Quantity Plot", "T. Average Integral", "Transverse Average", "Phase Space"]
+            items = ['Quantity Plot', 'T. Average Integral', 'Transverse Average', 'Phase Space']
         else:
             items = []
         
@@ -250,11 +250,11 @@ class LAVA_Qt(QMainWindow):
     def save_plot(self):
         file_dialog = QFileDialog()
         filepath, _ = file_dialog.getSaveFileName(
-            self, "Save Plot", "", "PNG Files (*.png);;PDF Files (*.pdf)"
+            self, 'Save Plot', '', 'PNG Files (*.png);;PDF Files (*.pdf)'
         )
         
         if filepath:
-            self.figure.savefig(filepath, dpi=800, bbox_inches="tight")
+            self.figure.savefig(filepath, dpi=800, bbox_inches='tight')
 
 def LAVA():
     app = QApplication(sys.argv)
@@ -262,5 +262,5 @@ def LAVA():
     window.show()
     sys.exit(app.exec())
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     LAVA()
