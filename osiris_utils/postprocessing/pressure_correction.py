92c8bb3324e7f0da34e062e144add7316af5f4cd
joaopedrobiu6 <joaopedrofbiu@tecnico.ulisboa.pt>
2026-04-20 10:11:19 +0100

    remove cache size limit enforcement in PressureCorrection_Diagnostic

--- osiris_utils/postprocessing/pressure_correction.py
+++ osiris_utils/postprocessing/pressure_correction.py
@@ -135,9 +135,6 @@ class PressureCorrection_Diagnostic(Diagnostic):
         result = P - n * u * v
         if cache is not None:
             cache[(index, data_slice)] = result
-            maxsize = getattr(self, "_frame_cache_maxsize", 8)
-            while len(cache) > maxsize:
-                cache.popitem(last=False)
         return result
 
 
